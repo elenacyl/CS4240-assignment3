886477227aa82007cc39f27bad3d16bd5fc03ecf
Elena Choo <e0035848@u.nus.edu>
2020-03-20 12:51:04 +0800

    add assignment 3

---
+++ Assets/Script/ButtonClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : MonoBehaviour
+{
+
+	public static string chosenButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chosenButton = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void selectButton()
+    {
+		chosenButton = EventSystem.current.currentSelectedGameObject.name;
+    }
+}
